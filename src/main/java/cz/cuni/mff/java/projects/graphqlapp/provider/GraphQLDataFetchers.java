ec5027cc480ec3ab6e766e32dce55bbe5dd01045
Jkuzz <honza.ku@gmail.com>
2022-04-19 17:44:11 +0200

    Removed books mock implementation

--- src/main/java/cz/cuni/mff/java/projects/graphqlapp/provider/GraphQLDataFetchers.java
+++ src/main/java/cz/cuni/mff/java/projects/graphqlapp/provider/GraphQLDataFetchers.java
@@ -9,31 +9,6 @@ public class GraphQLDataFetchers {
 
     private final GraphQLDataStore dataStore = new GraphQLDataStore();
 
-    public DataFetcher<Map<String, String>> getBookByIdDataFetcher() {
-        return dataFetchingEnvironment -> {
-            String bookId = dataFetchingEnvironment.getArgument("id");
-            return dataStore.getBooks()
-                    .stream()
-                    .filter(book -> book.get("id").equals(bookId))
-                    .findFirst()
-                    .orElse(null);
-        };
-    }
-
-    public DataFetcher<List<Map<String, String>>> getBooksByAuthorDataFetcher() {
-        return dataFetchingEnvironment -> {
-            String authorID = dataFetchingEnvironment.getArgument("authorId");
-            return dataStore.getBooks()
-                    .stream()
-                    .filter(book -> book.get("authorId").equals(authorID))
-                    .toList();
-        };
-    }
-
-    public DataFetcher<List<Map<String, String>>> getBooksDataFetcher() {
-        return dataFetchingEnvironment -> dataStore.getBooks();
-    }
-
     public DataFetcher<List<Map<String, String>>> getKrajeDataFetcher() {
         return dataFetchingEnvironment -> dataStore.getKraje();
     }
@@ -73,18 +48,5 @@ public class GraphQLDataFetchers {
             return null;
         };
     }
-
-    public DataFetcher<Map<String, String>> getAuthorDataFetcher() {
-        return dataFetchingEnvironment -> {
-            Map<String,String> book = dataFetchingEnvironment.getSource();
-            String authorId = book.get("authorId");
-            return dataStore.getAuthors()
-                    .stream()
-                    .filter(author -> author.get("id").equals(authorId))
-                    .findFirst()
-                    .orElse(null);
-        };
-    }
-
 
 }
